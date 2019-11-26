9b239f3484f3ec3e03ad1fda2a76d35852f662e6
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-11-26 04:09:14 +0100

    Make cache purging logic a bit more thorough

--- _releng/deploy.py
+++ _releng/deploy.py
@@ -21,7 +21,7 @@ def main():
     lines = listing.rstrip().split("\n")
     for line in lines:
         path = line[line.index(BUCKET_URI) + len(BUCKET_URI):]
-        urls.append(PUBLIC_URL + path)
+        urls += compute_urls_for_path(path)
 
     subprocess.check_call([
         "s3cmd",
@@ -34,6 +34,19 @@ def main():
 
     for batch in chop(urls, MAX_PURGES_PER_REQUEST):
         subprocess.check_call(["cfcli", "purge"] + batch)
+
+
+def compute_urls_for_path(path):
+    result = [PUBLIC_URL + path]
+
+    if path.endswith("/index.html"):
+        slash_index = path.rindex("/")
+        result += [
+            PUBLIC_URL + path[:slash_index],
+            PUBLIC_URL + path[:slash_index + 1]
+        ]
+
+    return result
 
 
 def chop(items, n):
